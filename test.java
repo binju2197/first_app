a702d78cec967d48ed7e6d0910760a3e83ae93aa
bin.ju <bin.ju@localhost.localdomain>
2016-12-18 15:32:35 +0800

    project2 modify

--- test.java
+++ test.java
@@ -3,3 +3,5 @@
 //project1: testing branch modificaton          3
 //project1: master  branch modification         4
 //project2: master  branch modificaton          5
+//project1: testing branch modificaton          6
+//project2: testing branch modificaton          7
